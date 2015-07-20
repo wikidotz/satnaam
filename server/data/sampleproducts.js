3e173c2dc4c2ba05e58e4d3a156c7a6f828f0ca8
mukeshgupta <kkrish123mg@gmail.com>
2015-07-20 16:30:39 +0530

    sample data files

--- server/data/sampleproducts.js
+++ server/data/sampleproducts.js
@@ -1,13 +1,13 @@
 [
 	{
-		"prod_id":1
+		"prod_id":1,
 		"prod_category_id":1,
 		"prod_name":"sevpuri",
-		"prod_dispname":"sp"
-		"prod_desc":"description"
+		"prod_dispname":"sp",
+		"prod_desc":"description",
 		"prod_ingredients":"abcxyz",
 		"prod_rate":30.00,
-		"prod_available":1
+		"prod_available":1,
 		"prod_size":null,
 		"prod_weight":null,
 		"prod_veg_nonveg":"veg",
@@ -16,14 +16,14 @@
 		"prod_modified_date":""
 	},
 	{
-		"prod_id":2
+		"prod_id":2,
 		"prod_category_id":1,
 		"prod_name":"bhelpuri",
-		"prod_dispname":"bhel"
-		"prod_desc":"description"
+		"prod_dispname":"bhel",
+		"prod_desc":"description",
 		"prod_ingredients":"abcxyz",
 		"prod_rate":30.00,
-		"prod_available":1
+		"prod_available":1,
 		"prod_size":null,
 		"prod_weight":null,
 		"prod_veg_nonveg":"veg",
