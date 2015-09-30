2196d34252ffb512e6aca305ac4c7bd70338dbe2
mukeshgupta <kkrish123mg@gmail.com>
2015-09-30 10:04:18 +0530

    saving order and items

--- client/js/services/order-service.js
+++ client/js/services/order-service.js
@@ -2,20 +2,6 @@ angular.module('hotelApp').service('Order',function($http){
 	
 	this.createNewOrder = function(orderObj){
 		logger().log("Saving order details-items");
-		/*orderObj = {
-			//"order_id":"1",
-			"cust_id":1,
-			"order_token_no":"1",
-			"order_table_no":1,
-			"order_total_amt":100.00,
-			"order_total_qty":2,
-			"order_expct_time":1200,
-			"order_date":"2015-07-20 13:16:00",
-			"order_status":1,
-			"order_pay_status":"0",
-			"order_mng_emp_id":"",
-		};*/
-		
 		return $http.put('/order/saveOrder',orderObj).then(function(response){
 			alert(response);
 		},
